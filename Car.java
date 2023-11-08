b507e58644cdf3df8cadb1aea5c379ef00ca4609
timpan21 <timnh1722@hotmail.com>
2023-11-08 12:40:43 +0100

    New Car

--- Car.java
+++ Car.java
@@ -1,16 +1,20 @@
 import java.awt.*;
 
-public abstract class Car {
+public abstract class Car implements Movable {
     protected double enginePower;
     private double currentSpeed;
     private Color color;
     private final String modelName;
+    private Point position;
+    private double direction;
 
     public Car(String modelName, double enginePower, Color color) {
         this.modelName = modelName;
         this.enginePower = enginePower;
         this.color = color;
         this.currentSpeed = 0;
+        this.position = new Point();
+        this.direction = 0.0;
     }
 
     public double getEnginePower() {
@@ -37,6 +41,22 @@ public abstract class Car {
         return modelName;
     }
 
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
     public void startEngine() {
         currentSpeed = 0.1;
     }
@@ -44,10 +64,11 @@ public abstract class Car {
     public void stopEngine() {
         currentSpeed = 0;
     }
-    
+
     public abstract void incrementSpeed(double amount);
+
     public abstract void decrementSpeed(double amount);
-    
+
     public void gas(double amount) {
         incrementSpeed(amount);
     }
@@ -55,4 +76,22 @@ public abstract class Car {
     public void brake(double amount) {
         decrementSpeed(amount);
     }
+
+    @Override
+    public void move() {
+        position.translate((int) (Math.cos(Math.toRadians(direction)) * currentSpeed),
+                           (int) (Math.sin(Math.toRadians(direction)) * currentSpeed));
+    }
+
+    @Override
+    public void turnLeft() {
+        direction = (direction - 90) % 360;
+    }
+
+    @Override
+    public void turnRight() {
+        direction = (direction + 90) % 360;
+    }
 }
+
+
